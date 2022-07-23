286628c2ff8cf6b39e3d11c6668adaad1662bbb1
gitanjali <gitaanjalide@gmail.com>
2022-07-24 00:31:00 +0530

    snake ladder game

---
+++ SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snakeLadder.game;
+
+public class SnakeLadder {
+	static  int START_POSITION=0;
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and ladder Game!!!");
+		}
+	}
